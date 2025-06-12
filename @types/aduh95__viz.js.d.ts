44e11a0dd91b9e74d4757f8f350cbec65c32d822
Len Damico <len@lendami.co>
2025-06-12 11:42:10 -0400

    Update bundle script to copy manifest.json and styles.css to output directory

---
+++ @types/aduh95__viz.js.d.ts
@@ -0,0 +1,12 @@
+declare module '@aduh95/viz.js/dist/index.mjs' {
+  const Viz: any;
+  export default Viz;
+}
+declare module '@aduh95/viz.js/dist/render.browser.js' {
+  const render: any;
+  export default render;
+}
+declare module '@aduh95/viz.js/worker' {
+  const render: any;
+  export default render;
+} 
